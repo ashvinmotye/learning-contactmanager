1707d414485650075303c925822e06f5aaa150d0
Ashvin Motye <22303668+ashvinmotye@users.noreply.github.com>
2019-05-31 20:24:35 +0400

    Add files via upload

---
+++ precache-manifest.da858d0685a4e4af9e31ccf4adb35085.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8235563eba5c5da7750009d1a3fd6862",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d7df286706c6c6ab1324",
+    "url": "/static/css/2.266e55a5.chunk.css"
+  },
+  {
+    "revision": "514b0723201923606dd7",
+    "url": "/static/css/main.36497136.chunk.css"
+  },
+  {
+    "revision": "d7df286706c6c6ab1324",
+    "url": "/static/js/2.baf3c1c8.chunk.js"
+  },
+  {
+    "revision": "514b0723201923606dd7",
+    "url": "/static/js/main.98fd0b14.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
